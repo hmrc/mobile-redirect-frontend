3af03769ae426e6ce1b444fc65cc95a91640b095
JavedBeg-ac <181977665+JavedBeg-ac@users.noreply.github.com>
2026-01-22 10:13:20 +0000

    HMA-11175 dependency upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,9 +2,9 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion    = "10.3.0"
-  private val playFrontendVersion = "12.19.0"
-  private val jsoupVersion        = "1.21.2"
+  private val bootstrapVersion    = "10.5.0"
+  private val playFrontendVersion = "12.28.0"
+  private val jsoupVersion        = "1.22.1"
 
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapVersion,
