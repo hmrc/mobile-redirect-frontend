7e2ba5a947e217aac27b27313360fe17e1dbe80e
JavedBeg-ac <181977665+JavedBeg-ac@users.noreply.github.com>
2026-04-14 16:04:41 +0100

    HMA-11518 dependency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,8 +2,8 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion    = "10.5.0"
-  private val playFrontendVersion = "12.29.0"
+  private val bootstrapVersion    = "10.7.0"
+  private val playFrontendVersion = "13.3.0"
   private val jsoupVersion        = "1.22.1"
 
   val compile = Seq(
